aa80be42a6e1b954722f3953faf0109d43b59683
kalana ranathunga <kalanaranathunga5@gmail.com>
2025-03-01 13:21:24 +0530

    gg

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author '6t9 studios'
+description 'A simple pet system for FiveM RP'
+version '1.0'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
